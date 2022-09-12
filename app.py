4b3096a0778d3101b9d2ba5c124febc2d0027e8b
Maciej Gruszczyński <maciek.gruszka@gmail.com>
2022-09-12 23:58:24 +0200

    feat: merge similar guidelines WIP

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from nltk.tokenize import word_tokenize
 import itertools
 
 nltk.download("punkt")
-
 spell = SpellChecker()
 
 st.title("Medium Headline")
@@ -23,8 +22,11 @@ headline = st.text_input(
     "",
 )
 
+# TODO keep merging guidelines
 # TODO Use columns or expander to organise that
 # TODO try to write a headline with that tool
+# TODO ask in Facebook/reddit group/privs Medium Wroters/blohgers for feedback!
+# TODO bloom few shot, BERT for surprise metric, train to paraphrase to specific types?
 # TODO maybe using markdown's H1-6 will result in better spacing
 # TODO split to the most important quidelined (answear question, few traits, auto editing features) and expandable/on button click advanced guidelines
 
@@ -34,29 +36,26 @@ st.checkbox("What the story is about?")
 st.checkbox("Why the story matters?")
 
 st.subheader("Make sure the headline is:")
-st.checkbox("clear")
-st.checkbox("direct")
-st.checkbox("assertive")
 st.checkbox("focused on what is most interesting")
-
-st.subheader("Consider below actions:")
-st.checkbox(
-    "Share the headline with a few writing peers and see what they think the story is about."
-    "When published, look at what people are saying when they share the article."
-)
-
-st.subheader("Follow below principles:")
-st.checkbox(
-    "Be direct.",
-    help="Your story is among many a reader is browsing. Be straightforward in what it is about.",
-)
-st.checkbox(
-    "Use conventional language.",
-    help="Avoid jargon, and think of what makes sense in casual conversation. Know the language that your audience is familiar with.",
-)
+    Is it interesting? Make it interesting
 st.checkbox(
     "Focus on what’s interesting.",
     help="Be straightforward about why a reader should read the article. Don’t bury or hide this.",
+)
+Focus on what’s interesting. Your headline doesn’t need to address everything that’s in your story. 
+It just needs to focus on the most interesting part of your story in a way that represents the overall truth of your story.
+
+st.subheader("Follow below principles:")
+st.checkbox( # (trait or individual style?)
+    "Be direct.",
+    help="Your story is among many a reader is browsing. Be straightforward in what it is about.",
+)
+"How To Become Popular Without Being Charming, Funny, or Outgoing"
+"How Google Earth Mapped 98% of the World"
+
+st.checkbox(
+    "Use conventional language.",
+    help="Avoid jargon, and think of what makes sense in casual conversation. Know the language that your audience is familiar with.",
 )
 st.checkbox(
     "Deliver on your promises.",
@@ -68,11 +67,11 @@ st.checkbox(
 )
 
 st.subheader("Guiding questions to consider:")
-st.checkbox("Could the headline be clearer?")
+st.checkbox("Make it clear. Could the headline be clearer?") # there is such type (is it rule along others or individual style)
 st.checkbox("Is the headline specific enough?")
 st.checkbox("Does the tone reflect the voice or point of view of the article?")
 st.checkbox("How might the headline convey what is unique about the story?")
-st.checkbox("Is the headline clear and honest about what the story offers the reader?")
+st.checkbox("Is it true? Make it true. Don't overpromise. Is the headline honest about what the story offers the reader?")
 
 st.subheader("Avoid:")
 st.checkbox("all caps")  # implement warning?
@@ -104,6 +103,9 @@ st.checkbox(
     "biases",
     help="“Be aware of who is in the room writing the headline and what the limits of their perspective might be,” shares Rawls. Often biases will show up in the adjectives you use, so double-check the ones you use, and consider what they may unintentionally express.",
 )
+"Easy on the gerunds (-ing words)." # TODO implement check
+"Don’t write a headline like you would a book title (avoid one-word or two-word headlines)." # TODO implement check
+#     Don’t ask a question unless you know the answer.
 
 "In the short term this may help drive traffic, but in the long term it undermines your integrity as a writer."
 
@@ -185,7 +187,41 @@ st.markdown(
 """
 )
 
-st.header("Sources:")
+Personal headlines
+    It's 2020, And I Had to Leave My Home Because of the KKK
+
+Validate readers’ feelings
+    In Defense of the Super Low-Key Workout
+
+Clear promises
+    How to Talk To an Employee Obsessed With Promotion
+    An Embarassing Story Is a Secret Weapon to Work
+
+Effective use of “I”
+    How I Accidentally Wound Up Running an Outlaw Bigger Gang in Ohio
+
+Effective uses of "you"
+    You Might Be Using Your Hearing and You Don't Even Know It
+    How to Pitch a 4-Day Workweek to Your Boss
+
+General guidelines
+
+    Don’t make the reader work. Or even think, really. Headlines should deliver all the information a reader needs for deciding whether they should dive into your story.
+    Use the right language for your audience. If you’re writing for a general audience, no jargon! Pick three friends: Would each of them know what your story is about based on the headline? If so, you have a great headline.
+    Answer: So what? Why now? Why does your story matter? Why does it matter to a reader right now?
+
+    Is it concise? Make it concise Omit unnecessary words
+    Is it assertive? Make it assertive (trait or individual style?)
+        Assertive headlines
+        Your Fitbit Might Be Able To Predict the Flu?
+    Would my really smart friend click on it?
+
+st.subheader("Consider below actions:") # OK
+st.checkbox("Share the headline with someone you trust and see what they think the story is about.")
+st.checkbox("Give them a few options and tell them to make a choice.")
+st.checkbox("When published, look at what people are saying when they share the article.")
+
+st.header("Sources:") # OK
 st.markdown(
     """[”How to Write a Headline” by Medium Creators](https://medium.com/creators-hub/how-to-write-a-headline-a72ab3449150)"""
 )
@@ -195,6 +231,6 @@ st.markdown(
 st.markdown(
     """[”23 Examples of Effective Headlines” by Nadia Rawls](https://medium.com/creators-hub/23-examples-of-effective-headlines-2e7f753476f1)"""
 )
-
-# TODO https://medium.com/creators-hub/how-to-write-a-compelling-headline-that-isnt-clickbait-7cb816cec438
-# some guidelines repeat
+st.markdown(
+    """[”How to Write a Compelling Headline That Isn’t Clickbait” by Medium Creators](https://medium.com/creators-hub/how-to-write-a-compelling-headline-that-isnt-clickbait-7cb816cec438)"""
+)
