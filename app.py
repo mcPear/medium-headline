95fb091f7377b8113e73dc5b704581860aeb2ca0
Maciej Gruszczyński <maciek.gruszka@gmail.com>
2022-09-15 11:19:48 +0200

    chore: organise content into 5 main categories

--- app.py
+++ app.py
@@ -9,56 +9,72 @@ nltk.download("punkt")
 spell = SpellChecker()
 
 st.title("Medium Headline")
-st.caption(
-    "The most important Medium Headline writing articles turned into an interactive app."
-)
-""
-""
-""
-""
+st.caption("Medium headline guidelines articles turned into an interactive app")
+
 
 st.header("Start with typing your headline idea")
-headline = st.text_input(
-    "",
-)
+headline = st.text_input("", "7 Steps How To NOT Make an Oak Coffee Table")
 
-# TODO keep merging guidelines
-# TODO Use columns or expander to organise that
-# TODO try to write a headline with that tool
-# TODO ask in Facebook/reddit group/privs Medium Wroters/blohgers for feedback!
-# TODO bloom few shot, BERT for surprise metric, train to paraphrase to specific types?
-# TODO maybe using markdown's H1-6 will result in better spacing
-# TODO split to the most important quidelined (answear question, few traits, auto editing features) and expandable/on button click advanced guidelines
+# TODO
+# """
+# MVP
+# - keep merging guidelines
+# - fill help when possible, for types too, clean up
+# - ask Danni for help
 
-st.header("Then follow below guidelines")
+# Workshoping:
+# - try to write a headline with that tool
+# - use columns or expander to organise that
+# - ask in Facebook/reddit group/privs Medium Wroters/bloggers for feedback!
+# - own domain (host on Streamlit)
+
+# Features:
+# - bloom few shot for high-level guidelines
+# - train paraphraser to specific types
+# - BERT for suprise metric
+
+# Design:
+# - check out H1-6 instead of Streamlit's header/subheader
+# """
+
+# 1 Substantial guidelines
+st.header("Substantial guidelines")
 st.subheader("Make sure your headline answers the ultimate questions:")
 st.checkbox("What the story is about?")
-st.checkbox("Why the story matters?")
+st.checkbox(
+    "Why the story matters to a reader?"
+)  # TODO does examplary headlines really answer that?
 
 st.subheader("Make sure the headline is:")
+
+### interesting
 st.checkbox("focused on what is most interesting")
-    Is it interesting? Make it interesting
+# Is it interesting? Make it interesting
 st.checkbox(
     "Focus on what’s interesting.",
     help="Be straightforward about why a reader should read the article. Don’t bury or hide this.",
 )
-Focus on what’s interesting. Your headline doesn’t need to address everything that’s in your story. 
-It just needs to focus on the most interesting part of your story in a way that represents the overall truth of your story.
+# Focus on what’s interesting. Your headline doesn’t need to address everything that’s in your story.
+# It just needs to focus on the most interesting part of your story in a way that represents the overall truth of your story.
+### interesting
 
-st.subheader("Follow below principles:")
-st.checkbox( # (trait or individual style?)
-    "Be direct.",
+st.checkbox(  # (trait or individual style?)
+    "Direct",
     help="Your story is among many a reader is browsing. Be straightforward in what it is about.",
 )
 "How To Become Popular Without Being Charming, Funny, or Outgoing"
 "How Google Earth Mapped 98% of the World"
 
-st.checkbox(
-    "Use conventional language.",
+st.checkbox(  # TODO it's optional depending on the audience
+    "Uses conventional language (if for a general audience)",
     help="Avoid jargon, and think of what makes sense in casual conversation. Know the language that your audience is familiar with.",
 )
 st.checkbox(
     "Deliver on your promises.",
+    help="You’re building a relationship with your readers. The headline sets the expectations, and the story must deliver on that.",
+)
+st.checkbox(
+    "Don’t ask a question unless you know the answer.",
     help="You’re building a relationship with your readers. The headline sets the expectations, and the story must deliver on that.",
 )
 st.checkbox(
@@ -67,28 +83,18 @@ st.checkbox(
 )
 
 st.subheader("Guiding questions to consider:")
-st.checkbox("Make it clear. Could the headline be clearer?") # there is such type (is it rule along others or individual style)
+st.checkbox(
+    "Make it clear. Could the headline be clearer?"
+)  # there is such type (is it rule along others or individual style)
 st.checkbox("Is the headline specific enough?")
 st.checkbox("Does the tone reflect the voice or point of view of the article?")
 st.checkbox("How might the headline convey what is unique about the story?")
-st.checkbox("Is it true? Make it true. Don't overpromise. Is the headline honest about what the story offers the reader?")
+st.checkbox(
+    "Is it true? Make it true. Don't overpromise. Is the headline honest about what the story offers the reader?"
+)
 
-st.subheader("Avoid:")
-st.checkbox("all caps")  # implement warning?
-st.checkbox("all typos")  # implement separately?
-if headline:
-    ll = [[word_tokenize(w), " "] for w in headline.split()]
-    tokens = list(itertools.chain(*list(itertools.chain(*ll))))
-    tokens = [
-        spell.correction(t) if spell.unknown([t]) and t != " " else t for t in tokens
-    ]
-    headline = "".join(tokens)
-    headline = titlecase(headline)
-    "Especially consider:"
-    st.info(headline)
-
-st.checkbox("links")  # implement warning with regex
-st.checkbox("profanity")  # implement?
+# TODO most of them below are extras/obvious
+st.checkbox("profanity")
 st.checkbox("exaggeration")
 st.checkbox("mystery")
 st.checkbox(
@@ -103,35 +109,43 @@ st.checkbox(
     "biases",
     help="“Be aware of who is in the room writing the headline and what the limits of their perspective might be,” shares Rawls. Often biases will show up in the adjectives you use, so double-check the ones you use, and consider what they may unintentionally express.",
 )
-"Easy on the gerunds (-ing words)." # TODO implement check
-"Don’t write a headline like you would a book title (avoid one-word or two-word headlines)." # TODO implement check
-#     Don’t ask a question unless you know the answer.
 
-"In the short term this may help drive traffic, but in the long term it undermines your integrity as a writer."
-
-st.subheader("Viral tips:")
 st.checkbox(
     "The key to writing a tempting benefit-based headline is to be specific.",
     help="“How to Wake Up Smiling: The 9 Decisions That Led To A Life I Love”\n“This 3-Minute Exercise Will Change the Way You Solve Problems”\n“How I Doubled My Writing Income Overnight By Focusing on One Skill”",
 )
-st.checkbox(
-    "Negative headlines outperform positive ones.",
-    help="“Six Habits of Deeply Happy People“\n“7 Things You Should Do in the Morning“\n“A Rarely Mentioned Relationship Virtue That Strengthens Couples“\n\nV.S\n\n“Six Habits of Deeply Miserable People”\n“7 Things You Should Never Do in the Morning“\n“A Rarely Mentioned Relationship Sin That Bulldozes Couples”",
-)
-st.checkbox(
-    "Offer unconventional wisdom.",
-    help="“Six Habits of Deeply Happy People“\n“7 Things You Should Do in the Morning“\n“A Rarely Mentioned Relationship Virtue That Strengthens Couples“\n\nV.S\n\n“Six Habits of Deeply Miserable People”\n“7 Things You Should Never Do in the Morning“\n“A Rarely Mentioned Relationship Sin That Bulldozes Couples”",
-)
-st.checkbox(
-    "Make it controversial and divisive.",
-    help="Here are a few perfect examples:\n“You Will Destroy Yourself Financially If You Save”\n“Pepsi’s $32 Billion Typo Caused Deadly Riots”\n“11 Things Socially Aware People Don’t Say”",
-)  # TODO explain it is in opposite to Meodum guidelines
-st.checkbox(
-    "Share practical life advice that puts Universities to shame.",
-    help="“A Behind the Scenes Look at My Writing Schedule That’s Helped Write 5000+ Articles”\n“Steve Jobs Advice Turned This Programmer Into A Billionaire”\n“8 Really Small Things That Tell You a Lot About Someone”",
-)
 
-st.subheader("Headline categories for inspiration")
+# 2 Editorial guidelines
+
+st.subheader("Avoid:")
+st.checkbox("all caps")  # implement warning?
+# alpha_chars = [c for c in headline if c.isalpha()]
+# upper_chars = [c for c in alpha_chars if c.isupper()]
+# upper_ratio = upper_chars / alpha_chars
+# is_mostly_upper = upper_ratio > 0.8
+# TODO show is_mostly_upper
+
+st.checkbox("all typos")  # red -> green, Spacy
+# if headline:
+#     ll = [
+#         [word_tokenize(w), " "] for w in headline.split()
+#     ]  # TODO split whitespaces with regex to keep full whitespaces
+#     tokens = list(itertools.chain(*list(itertools.chain(*ll))))
+#     tokens = [
+#         spell.correction(t) if spell.unknown([t]) and t != " " else t for t in tokens
+#     ]
+#     headline = "".join(tokens)
+#     headline = titlecase(headline)
+#     "Especially consider:"
+#     st.info(headline)
+
+st.checkbox("links")  # implement warning with regex
+"Easy on the gerunds (-ing words)." "People are 70% more likely to click on the nongerund headlines."  # TODO implement check
+"Don’t write a headline like you would a book title (avoid one-word or two-word headlines)."  # TODO implement check
+
+# 4 Proven Headline Types OK
+
+st.subheader("Proven Headline Types")
 st.write("**Analysis**")
 st.markdown(
     """
@@ -187,41 +201,112 @@ st.markdown(
 """
 )
 
-Personal headlines
-    It's 2020, And I Had to Leave My Home Because of the KKK
+st.write("**Personal headlines**")
+st.markdown(
+    """
+- It's 2020, And I Had to Leave My Home Because of the KKK
+"""
+)
 
-Validate readers’ feelings
-    In Defense of the Super Low-Key Workout
+st.write("**Validate readers’ feelings**")
+st.markdown(
+    """
+- In Defense of the Super Low-Key Workout
+"""
+)
 
-Clear promises
-    How to Talk To an Employee Obsessed With Promotion
-    An Embarassing Story Is a Secret Weapon to Work
+st.write("**Clear promises**")
+st.markdown(
+    """
+- How to Talk To an Employee Obsessed With Promotion
+- An Embarassing Story Is a Secret Weapon to Work
+"""
+)
 
-Effective use of “I”
-    How I Accidentally Wound Up Running an Outlaw Bigger Gang in Ohio
+st.write("**Effective use of “I”**")
+st.markdown(
+    """
+- How I Accidentally Wound Up Running an Outlaw Bigger Gang in Ohio
+"""
+)
 
-Effective uses of "you"
-    You Might Be Using Your Hearing and You Don't Even Know It
-    How to Pitch a 4-Day Workweek to Your Boss
+st.write('**Effective uses of "you"**')
+st.markdown(
+    """
+- You Might Be Using Your Hearing and You Don't Even Know It
+- How to Pitch a 4-Day Workweek to Your Boss
+"""
+)
 
-General guidelines
+st.write("**Negative headlines**")
+st.write("Tip: Negative headlines often outperform positive ones")
+st.markdown(
+    """
+- Six Habits of Deeply Happy People -> Six Habits of Deeply Miserable People
+- 7 Things You Should Do in the Morning -> 7 Things You Should Never Do in the Morning
+- A Rarely Mentioned Relationship Virtue That Strengthens Couples -> A Rarely Mentioned Relationship Sin That Bulldozes Couples
+"""
+)
 
-    Don’t make the reader work. Or even think, really. Headlines should deliver all the information a reader needs for deciding whether they should dive into your story.
-    Use the right language for your audience. If you’re writing for a general audience, no jargon! Pick three friends: Would each of them know what your story is about based on the headline? If so, you have a great headline.
-    Answer: So what? Why now? Why does your story matter? Why does it matter to a reader right now?
+st.write("**Offer unconventional wisdom.**")
+st.markdown(
+    """
+- Your Life Is Full of Porn. Stop Getting Yourself Off
+- How to Quietly Get People’s Attention in a Noisy World
+- Lessons from a Billionaire Who Can’t Focus on Anything for More than 4 Hours
+"""
+)
 
-    Is it concise? Make it concise Omit unnecessary words
-    Is it assertive? Make it assertive (trait or individual style?)
-        Assertive headlines
-        Your Fitbit Might Be Able To Predict the Flu?
-    Would my really smart friend click on it?
+st.write(
+    "**Controversial and divisive**"
+)  # TODO maybe remove, validate with other rules
+st.markdown(
+    """
+- You Will Destroy Yourself Financially If You Save
+- Pepsi’s $32 Billion Typo Caused Deadly Riots
+- 11 Things Socially Aware People Don’t Say
+"""
+)
 
-st.subheader("Consider below actions:") # OK
-st.checkbox("Share the headline with someone you trust and see what they think the story is about.")
+st.write("**Assertive**")
+st.markdown(
+    """
+- Can Your Fitbit Predict the Flu? -> Your Fitbit Might Be Able to Predict the Flu
+- Alaska's Swimsuit Scandal Unfairly Polices Young Girls' Bodies
+- Meghan Markle Defeated the British Monarchy
+"""
+)
+
+st.write("**Concise**")
+st.markdown(
+    """
+- Your Office Chair Is Hurting You
+- Our Skulls Are Out-Evolving Us
+- Yes, She's Cheating On You
+"""
+)
+
+st.write("**Share practical life advice that puts Universities to shame**")
+st.markdown(
+    """
+- A Behind the Scenes Look at My Writing Schedule That’s Helped Write 5000+ Articles
+- Steve Jobs Advice Turned This Programmer Into A Billionaire
+- 8 Really Small Things That Tell You a Lot About Someone
+"""
+)
+
+# 4 Actions DONE
+st.subheader("Actions:")
+st.checkbox(
+    "Share the headline with someone you trust and see what they think the story is about."
+)
 st.checkbox("Give them a few options and tell them to make a choice.")
-st.checkbox("When published, look at what people are saying when they share the article.")
+st.checkbox(
+    "When published, look at what people are saying when they share the article."
+)
 
-st.header("Sources:") # OK
+# 5 Sources DONE
+st.subheader("Sources:")
 st.markdown(
     """[”How to Write a Headline” by Medium Creators](https://medium.com/creators-hub/how-to-write-a-headline-a72ab3449150)"""
 )
